ed30b7f8474e83b5c0521943e898d6248896a3a5
hideki <hideki.1516.web@gmail.com>
2022-02-16 22:47:29 +0900

    Axios追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios  from 'axios';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 const App = () => {
-    const onSearchSubmit = (term) => {
-        console.log(term);
+    // const onSearchSubmit = (term) => {
+    //     console.log(term);
+    // };
+
+    const [images, setImages] = useState([]);
+
+    const onSearchSubmit = async(term) => {
+        try{
+            const params = {
+                key: "25733271-08c1e7594eeaa881e2b4d1ea2",
+                q: term,
+            };
+            const response = await axios.get("https://pixabay.com/api/", {params});
+            setImages(response.data.hits);
+            if(response.data.total === 0) {
+                alert('お探しの画像はありません。')
+            }
+        } catch {
+            alert('写真の取得に失敗しました。')
+        }
     };
 
     return (
         <div className='ui container' style={{marginTop: '20px'}}>
             {/* onSearchSubmit関数をpropsとして渡す */}
             <SearchBar onSubmit={onSearchSubmit} />
+            <ImageList images={images} />
         </div>
     );
 };
